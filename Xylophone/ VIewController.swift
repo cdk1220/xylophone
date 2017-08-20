6a7ed2594f9a834dcae2456e16d04c2854b617d8
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2017-08-20 13:32:09 -0500

    Implemented functionality to play the relevant soundfile when a key is pressed and that marked the end of the project

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -11,7 +11,8 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    var audioPlayer : AVAudioPlayer!
+    var audioPlayer: AVAudioPlayer!
+    let notes = ["note1", "note2", "note3", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,14 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        let soundURL = Bundle.main.url(forResource: "note2", withExtension: "wav")
+        playSound(fileName: notes[sender.tag - 1])
+    }
+    
+    func playSound(fileName: String) {
+        let soundURL = Bundle.main.url(forResource: fileName, withExtension: "wav")
         
         do {
-            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
         }
         catch {
             print(error)
@@ -33,7 +37,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.play()
         
     }
-    
   
 
 }
